7eb5e34b980cbcf5fccdc1d4cfa404e487a55ed6
alihassanfullstackdev-code <alihassan.fullstack.dev@gmail.com>
2026-05-06 05:37:05 +0500

    Update cors.php

--- gold-silver-jewellery-backend/config/cors.php
+++ gold-silver-jewellery-backend/config/cors.php
@@ -23,7 +23,8 @@ return [
         'https://gold-silver-jewellers-production.up.railway.app',
         'http://localhost:3000',
         // 'http://localhost:5173',
-        'http://127.0.0.1:3000'
+        'http://127.0.0.1:3000',
+        '*',
     ],
 
     'allowed_origins_patterns' => [],
