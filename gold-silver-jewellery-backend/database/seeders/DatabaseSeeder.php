6a6adcbecc9c3a8ccfa5023e6ca8ba298d550f26
alihassanfullstackdev-code <alihassan.fullstack.dev@gmail.com>
2026-05-09 16:40:52 +0500

    Update DatabaseSeeder.php

--- gold-silver-jewellery-backend/database/seeders/DatabaseSeeder.php
+++ gold-silver-jewellery-backend/database/seeders/DatabaseSeeder.php
@@ -3,23 +3,20 @@
 namespace Database\Seeders;
 
 use App\Models\User;
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
 
 class DatabaseSeeder extends Seeder
 {
-    use WithoutModelEvents;
-
     /**
      * Seed the application's database.
      */
     public function run(): void
     {
-        // User::factory(10)->create();
-
         User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
+            'name' => 'Admin User',
+            'email' => 'admin@gsj.com',
+            'password' => Hash::make('GSJ123@'),
         ]);
     }
 }
