b7ad07d136c130dd05917fce00155eab4d35a2c8
alihassanfullstackdev-code <alihassan.fullstack.dev@gmail.com>
2026-05-12 03:46:53 +0500

    Update providers.php

--- gold-silver-jewellery-backend/bootstrap/providers.php
+++ gold-silver-jewellery-backend/bootstrap/providers.php
@@ -1,9 +1,7 @@
 <?php
 
-use App\Providers\AppServiceProvider;
-
 return [
-    AppServiceProvider::class,
-    bSecure\UniversalCheckout\CheckoutServiceProvider::class,
-    bSecure\UniversalCheckout\SSOServiceProvider::class,
+    App\Providers\AppServiceProvider::class,
+    \bSecure\UniversalCheckout\CheckoutServiceProvider::class,
+    \bSecure\UniversalCheckout\SSOServiceProvider::class,
 ];
