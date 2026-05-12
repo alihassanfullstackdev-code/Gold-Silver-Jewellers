a60ca507afcab7b0792e6eb52a4bdb9d1c3fa5ee
alihassanfullstackdev-code <alihassan.fullstack.dev@gmail.com>
2026-05-13 04:52:50 +0500

    Update Order.php

--- gold-silver-jewellery-backend/app/Models/Order.php
+++ gold-silver-jewellery-backend/app/Models/Order.php
@@ -6,5 +6,6 @@ use Illuminate\Database\Eloquent\Model;
 
 class Order extends Model
 {
-    //
+    protected $fillable = ['order_id', 'order_reference', 'customer_email', 'total_amount', 'status', 'cart_details'];
+    protected $casts = ['cart_details' => 'array'];
 }
