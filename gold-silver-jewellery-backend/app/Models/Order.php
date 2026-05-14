eca2e959b00e7c66d910e1b9cf9dfe27b5cf9e34
alihassanfullstackdev-code <alihassan.fullstack.dev@gmail.com>
2026-05-14 23:08:34 +0500

    Update Order.php

--- gold-silver-jewellery-backend/app/Models/Order.php
+++ gold-silver-jewellery-backend/app/Models/Order.php
@@ -9,13 +9,16 @@ class Order extends Model
     protected $fillable = [
         'order_id',
         'order_reference',
+        'customer_name',
         'customer_email',
+        'customer_phone',
+        'customer_address',
         'total_amount',
         'status',
         'cart_details'
     ];
 
     protected $casts = [
-        'cart_details' => 'array'
+        'cart_details' => 'json',
     ];
 }
